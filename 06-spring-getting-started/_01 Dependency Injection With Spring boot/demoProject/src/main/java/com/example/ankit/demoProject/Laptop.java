3d1cdc59e5c2825b427eec0e9cb4e1137982d2b8
Ankitgujare <ankitgujare001@gmail.com>
2026-04-26 23:41:16 +0530

    AutoWired in Springboot

---
+++ 06-spring-getting-started/_01 Dependency Injection With Spring boot/demoProject/src/main/java/com/example/ankit/demoProject/Laptop.java
@@ -0,0 +1,10 @@
+package com.example.ankit.demoProject;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+    void coding(){
+        System.out.println("Coding Using Laptop");
+    }
+}
--- 06-spring-getting-started/_01 Dependency Injection With Spring boot/demoProject/src/main/java/com/example/ankit/demoProject/student.java
+++ 06-spring-getting-started/_01 Dependency Injection With Spring boot/demoProject/src/main/java/com/example/ankit/demoProject/student.java
@@ -1,11 +1,15 @@
 package com.example.ankit.demoProject;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class student {
+    @Autowired
+    Laptop laptop;
+
     void coding(){
-        System.out.println("Coding...");
+        laptop.coding();
     }
 }
