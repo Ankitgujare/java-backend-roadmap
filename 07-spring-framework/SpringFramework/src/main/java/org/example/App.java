b20b603f6062f44fbda1f4b54e385d8bd6ef11db
Ankitgujare <ankitgujare001@gmail.com>
2026-04-29 23:54:47 +0530

    started with Spring Framwork

---
+++ 07-spring-framework/SpringFramework/src/main/java/org/example/App.java
@@ -0,0 +1,27 @@
+package org.example;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+
+        //xml based config
+        // it creates A container for us
+        ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
+
+
+        Student s1= (Student) context.getBean("student");
+        s1.coding();
+
+        //it will work but we are using Spring right
+        //it should create an object for us..But we are doing it Currently
+        //Do to get the Object with SpringFramework
+
+        //and we need to create that Ioc Container
+
+    }
+}
---
+++ 07-spring-framework/SpringFramework/src/main/java/org/example/Student.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Student {
+
+    void coding(){
+        System.out.println("Coding...");
+    }
+
+
+}
