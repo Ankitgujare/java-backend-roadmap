54a25dd035378bda7697d755a1a2902c36e254bd
Ankitgujare <ankitgujare001@gmail.com>
2026-06-20 19:31:15 +0530

    Sending response to Client

--- 11-spring-boot-web/servletDemo/src/main/java/org/example/HellowServlet.java
+++ 11-spring-boot-web/servletDemo/src/main/java/org/example/HellowServlet.java
@@ -1,16 +1,22 @@
 package org.example;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class HellowServlet  extends HttpServlet {
 
 
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        PrintWriter write =resp.getWriter();
+        resp.setContentType("text/html");
+        write.println("<b><h1>Hello from servlet</b><h1>");
         System.out.println("In service");
     }
 }
